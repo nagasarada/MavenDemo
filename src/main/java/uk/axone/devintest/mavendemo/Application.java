ec74d973d716773a4c92c2eae5ded35bbecd271a
nagasarada <lillymaster10@gmail.com>
2023-06-01 07:27:36 +0100

    MavenDemo

---
+++ src/main/java/uk/axone/devintest/mavendemo/Application.java
@@ -0,0 +1,7 @@
+package uk.axone.devintest.mavendemo;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("in main src");
+    }
+}
---
+++ src/test/java/uk/axone/devintest/maventests/TestPack1.java
@@ -0,0 +1,24 @@
+package uk.axone.devintest.maventests;
+
+
+import org.junit.Test;
+
+public class TestPack1 {
+    @Test
+    public void test1(){
+        System.out.println("running test1");
+    }
+    @Test
+    public void test2(){
+        System.out.println("running test2");
+    }
+    @Test
+    public void test3(){
+        System.out.println("running test3");
+    }
+
+
+
+
+
+}
